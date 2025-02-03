9eb742b2dfca611217f6d56b6286aa9d88b4ee19
franquesino <you@example.com>
2025-02-03 05:16:15 -0300

    Primer commit

---
+++ app/src/main/java/com/example/franco_rojas_seccioncurso/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.franco_rojas_seccioncurso
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.franco_rojas_seccioncurso.ui.theme.Franco_rojas_seccioncursoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Franco_rojas_seccioncursoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Franco_rojas_seccioncursoTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/franco_rojas_seccioncurso/SplashScreenActivity.kt
@@ -0,0 +1,62 @@
+package com.example.franco_rojas_seccioncurso
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.*
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+
+class SplashScreenActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            SplashScreen {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }
+        }
+    }
+}
+
+@Composable
+fun SplashScreen(onAnimationEnd: () -> Unit) {
+    val compositionResult = rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.my_retail_animation))
+    val composition = compositionResult.value
+
+    val animationState = animateLottieCompositionAsState(composition)
+    val progress = animationState.progress
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        LottieAnimation(
+            composition = composition,
+            progress = progress,
+            modifier = Modifier.size(300.dp)
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(onClick = onAnimationEnd) {
+            Text(text = "Entrar a la aplicación")
+        }
+    }
+}
